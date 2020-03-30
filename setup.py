4e91267a1bd9ef47887ae6f40786f3fb04d6248c
smartcaveman <smartcaveman@gmail.com>
2020-03-30 18:05:47 -0400

    bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kaggle-storage-client",
-    version="0.0.1",
+    version="0.1.0",
     author="smartcaveman",
     author_email="smartcaveman@gmail.com",
     description="Provides a simplified interface for using kaggle datasets as a remote data file storage.",
